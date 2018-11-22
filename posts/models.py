900e82e7532c6d2c0b8878a6c0432ba3a438498e
Kibaek Lee <lkbhiphop@gmail.com>
2018-11-22 05:55:48 +0000

    imageall

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+def post_image_path(instance, filename):
+    return 'posts/{}/{}'.format(instance.pk, filename)
+    # filename은 이름이다.
 
 # Create your models here.
 class Post(models.Model):
-    image = models.ImageField()
+    image = ProcessedImageField(upload_to='posts/images',
+    processors=[ResizeToFill(300,300)],
+    format='JPEG',
+    options={'quality':90},
+    )
     content = models.TextField()
+    
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'pk':self.pk})
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import  reverse_lazy
 
 # Create your views here.
 class PostList(ListView):
     model = Post
+    
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['image','content',]
+    
+
+class PostDetail(DetailView):
+    model = Post
+    
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['image','content',]
+    
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:list')
